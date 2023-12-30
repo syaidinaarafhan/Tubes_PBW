5b295955451888a8814c5bea6481cea3bf5543d1
FadhlanJamil <fadhlanjamil6@gmail.com>
2023-12-30 13:24:49 +0700

    push

--- resources/views/GalangDana/page.blade.php
+++ resources/views/GalangDana/page.blade.php
@@ -1,29 +1,29 @@
 <x-app-layout> 
-    <div class="ml-80 pl-20 flex pt-10 pb-5 bg-stone-300 w-min">
+    <div class="ml-80 pl-20 flex pt-5 pb-5 bg-stone-300 w-min rounded-full">
         <!-- <div class="flex-none w-40 h-10 bg-yellow-400 rounded-full"> -->
-            <div class="flex-none w-40 h-10 bg-yellow-400 rounded-full">
+            <div class="flex-none w-40 h-10 ">
                 <button class="bg-red-300 rounded-full w-10 h-10" >1</button>  
             </div>
-            <div class="flex-none w-40 h-10 bg-yellow-400 rounded-full">
+            <div class="flex-none w-40 h-10 ">
                 <button class="bg-red-300 rounded-full w-10 h-10" >2</button>  
             </div>
-            <div class="flex-none w-40 h-10 bg-yellow-400 rounded-full">
+            <div class="flex-none w-40 h-10 ">
                 <button class="bg-red-300 rounded-full w-10 h-10" >3</button>  
             </div>
-            <div class="flex-none w-40 h-10 bg-yellow-400 rounded-full">
+            <div class="flex-none w-40 h-10 ">
                 <button class="bg-red-300 rounded-full w-10 h-10" >4 </button>  
             </div>
-            <div class="flex-none w-40 h-10 bg-yellow-400  rounded-full ">
+            <div class="flex-none w-32 h-10 ">
                 <button class="bg-red-300 rounded-full w-10 h-10" >5 </button>   
             </div>
         <!-- </div> -->
     </div>
     <div class="ml-64 bg-stone-100 w-2/3 sm:rounded-lg ">
-        <div class="text-2xl text-center pt-10">
+        <div class="text-2xl text-center pt-5">
             <p>Apakah Anda Menyutujui Syarat  dan ketentuan untuk Membuka Galang Dana</p>
         </div>
         <div class="text-lg ml-40 " >
-            <div class="mt-10">
+            <div class="mt-5">
                 <p>Pemilik rekening bertanggung jawab atas penggunaan dana yang diterima dari galang dana ini</p>
             </div>
             <div class="mt-5 ">
